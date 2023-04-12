001d6bdb49d6533f8713bafafeba9ec65cb88094
Lorenzo3281 <lorenzoa@terpmail.umd.edu>
2023-04-12 17:21:07 -0400

    Creation of functions
    Created the structure of the final project

---
+++ final.py
@@ -0,0 +1,61 @@
+def scrapeData(url):
+        ''''
+        Scrape data from website and return movie data used in program
+        '''
+        return
+
+class Movie:
+    '''
+    Movie class that stores data for the particular movie
+    '''
+    def __init__(self, title, genre, summary, actors):
+        '''
+        Initialization of the attributes used in Movie class
+        '''
+        return
+    
+    def extractTitles(title):
+        '''
+        Scrapes database for movie titles and return movie titles
+        '''
+    
+    
+        return
+    
+    def extractGenres(genre):
+        '''
+        Scrapes database for movie genres returns movies of similar genres
+        '''
+    
+        return
+    
+    def Summary(summary):
+        '''
+        #Scrapes database for corresponding movie descriptions and returns the movie descriptions
+        '''
+        return
+
+    def extractMovieActors(actors):
+        '''
+        Scrapes the actors in the movie titles
+        '''
+        return
+    
+def userChoice():
+    '''
+    get user preferences search by genre or actor
+    user input
+    '''
+    return choice
+
+def movieRecs(choice, title, genre, actor):
+    '''
+    Movie
+    ''' 
+    return
+
+def main():
+    '''
+    main method
+    '''
+    return
