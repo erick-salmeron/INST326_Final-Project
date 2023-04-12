ed3a6ba4f3e92fd3fa622c83ffdfa034ce10e42b
Melvin5808 <127872703+Melvin5808@users.noreply.github.com>
2023-04-12 18:07:18 -0400

    Added docstrings
    Added complete docstrings to each function

--- final.py
+++ final.py
@@ -1,61 +1,120 @@
+""" INST326 Final Project Deliverable
+
+Purpose: Having trouble finding a movie to watch? Use this program that 
+         will recommmend you different movies based on your search 
+         preferance of searching by genre or by actor.
+
+"""
+
 def scrapeData(url):
-        ''''
-        Scrape data from website and return movie data used in program
-        '''
-        return
+    """ Scrape data from website and return movie data
+    
+    Args:
+        url (str): The website URL that is being scraped
+    
+    Returns:
+        data (list): A list of movie data extracted from the website
+    """
+    return
 
 class Movie:
-    '''
-    Movie class that stores data for the particular movie
-    '''
+    """ A Movie class that stores data for a particular movie
+    
+    Attributes:
+        title (str): The title of the movie
+        genre (str): The genre of the movie
+        summary (str): The summary/description of the movie
+        actors (list): A list of actors starring in the movie
+
+    """
+    
     def __init__(self, title, genre, summary, actors):
-        '''
-        Initialization of the attributes used in Movie class
-        '''
+        """ Initializes the attributes used for the Movie class
+        
+        Args:
+            title (str): The title of the movie
+            genre (str): The genre of the movie
+            summary (str): The summary/description of the movie
+            actors (list): A list of actors in the movie
+
+        """
         return
     
     def extractTitles(title):
-        '''
-        Scrapes database for movie titles and return movie titles
-        '''
-    
-    
+        """ Extracts movie titles from url based on if it fits the genre or actor
+        
+        Args:
+            title (str): The title that is being extracted
+        
+        Returns:
+            titles (list): A list of movie titles that fit the genre or actor searched for
+
+        """
         return
     
     def extractGenres(genre):
-        '''
-        Scrapes database for movie genres returns movies of similar genres
-        '''
+        """ Extracts movies with same genres from a url
+        
+        Args:
+            genre (str): The genre to that is being searched for
+        
+        Returns:
+            genre (list): A list of movies that have the same genre searched for 
+
+        """
     
         return
     
-    def Summary(summary):
-        '''
-        #Scrapes database for corresponding movie descriptions and returns the movie descriptions
-        '''
+    def extractSummary(summary):
+        """ Extracts movie summaries from url
+        
+        Args:
+            summary (str): The summary of the movie
+        
+        Returns:
+            summary (str): The summary of the movie
+            
+        """
         return
 
     def extractMovieActors(actors):
-        '''
-        Scrapes the actors in the movie titles
-        '''
+        """ Extracts the actors in the movie from url parsed
+        
+        Args:
+            actors (str): The name of the actor that is being looked for from the url
+        
+        Returns:
+            actors (list): A list of movies that match the actor which is picked
+
+        """
         return
     
 def userChoice():
-    '''
-    get user preferences search by genre or actor
-    user input
-    '''
+    """ Prompts the user to choose their search preference of genre or actor
+    
+    Returns:
+        choice (str): The user's search preference
+
+    """
     return choice
 
 def movieRecs(choice, title, genre, actor):
-    '''
-    Movie
-    ''' 
+    """ Recommends movies based on user's choice of genre or actor
+    
+    Args:
+        choice (str): The user's search choice of genre or actor
+        title (str): The movie title searched for
+        genre (str): The movie genre searched for
+        actor (str): The actor searched for
+    
+    Returns:
+        recs (list): A list of recommended movies that match the users choice
+
+    """
     return
 
 def main():
-    '''
-    main method
-    '''
+    """ The main method pulls all of the methods and functions to runs the code
+
+    """
     return
