156c0befb7c003a368449a6a926130a950adadfb
Vineet Ravichandran <96751699+raanvine@users.noreply.github.com>
2023-04-27 16:45:44 -0400

    Update final.py
    working updated code(still need comments and unit tests)

--- final.py
+++ final.py
@@ -1,232 +1,103 @@
-""" INST326 Final Project Deliverable
-
-Purpose: Having trouble finding a movie to watch? Use this program that
-         will recommmend you different movies based on your search
-         preferance of searching by genre or by actor.
-
-"""
 import requests
-import unittest
 from bs4 import BeautifulSoup
+import time
+import unittest
 
-def scrape_movies(url):
-    resp = requests.get(url)
-    s = BeautifulSoup(resp.content, 'html.parser')
-    titles = [t.get_text() for t in s.select('.title a')]
-    genres = [g.get_text() for g in s.select('.genre')]
-    descrips = [d.get_text() for d in s.select('.description')]
-    actors = [a.get_text() for a in s.select('.actors')]
-    return resp,s,titles, genres, descrips, actors
 
 class Movie:
-    """ A Movie class that stores data for a particular movie
-
-    Attributes:
-        title (str): The title of the movie
-        genre (str): The genre of the movie
-        summary (str): The summary/description of the movie
-        actors (list): A list of actors starring in the movie
-
-    """
+    """A Movie Class that stores data for a particular movie"""
 
     def __init__(self, title, genre, summary, actors):
-        """ Initializes the attributes used for the Movie class
-
-        Args:
-            title (str): The title of the movie
-            genre (str): The genre of the movie
-            summary (str): The summary/description of the movie
-            actors (list): A list of actors in the movie
-
-        """
         self.title = title
         self.genre = genre
         self.summary = summary
         self.actors = actors
 
-    def extractTitles(title):
-        """ Extracts movie titles from url based on if it fits the genre or actor
-
-        Args:
-            title (str): The title that is being extracted
-
-        Returns:
-            titles (list): A list of movie titles that fit the genre or actor searched for
-
-        """
-    titles = []  # empty list of movie titles
-    for t in s.select('.Movie.Title a'):  # uses for loop to see which elements have same CSS selector
-        titles.append(t.get_text().strip())  # extracts the text content and then appends it to the titles list
-    return titles  # returns the list
-
-    def extractGenres(genre):
-        """ Extracts movies with same genres from a url
-
-        Args:
-            genre (str): The genre to that is being searched for
-
-        Returns:
-            genre (list): A list of movies that have the same genre searched for
-
-        """
-        genre = []
-        for movie in scrapeData(url):
-            if title in movie.genre:
-                genre.append(movie)
-        return genre
+    def __str__(self):
+        return f"{self.title} ({self.genre}) - {self.summary}\nLead Actor: {self.actors}\n"
 
 
-    def extractSummary(summary):
-        """ Extracts movie summaries from url
+    def scrape_movies(genre):
+        """Scrapes data from Imdb database"""
+        movies = [] #empty list of movie objects
+        url = 'https://www.imdb.com/search/title/?title_type=feature&genres={}&start={}' #url for scraping data
+        databasepage = 1
+        while databasepage <= 10: #loop through databse up to 10th page
+            resp = requests.get(url.format(genre, (databasepage-1)*50)) #request to url with genre and page
+            if resp.status_code != 200: #if no response is gotten, we break
+                break
+            resp = requests.get(url.format(genre, databasepage*50)) #sends another request with the next page
+            s = BeautifulSoup(resp.content, 'html.parser') #parses the HTML content
+            #extracts this information using CSS selectors
+            titles = [tit.get_text().strip() for tit in s.select('.lister-item-header a')]
+            genres = [gen.get_text().strip() for gen in s.select('.genre')]
+            summaries = [summ.get_text().strip() for summ in s.select('.ratings-bar+ .text-muted')]
+            actors = [act.get_text().strip() for act in s.select('.lister-item-content .ghost + a')]
+            #makes a movie object for each movie and adds it to the list
+            movies += [Movie(tit, gen, summ, act) for tit, gen, summ, act in zip(titles, genres, summaries, actors)]
+            if s.select('.lister-page-next.disabled'): #if there isnt a next page button, break the loop
+                break
+            databasepage+=1 #increment page and wait 5 seconds before going to the next one and scraping it
+            time.sleep(5)
 
-        Args:
-            summary (str): The summary of the movie
+        return movies #return list
 
-        Returns:
-            summary (str): The summary of the movie
 
-        """
-        descriptions = []
-        for description in scrapeData(url):
-            if summary in description.summary:
-                descriptions.append(description)
-        return descriptions
 
-    def extractMovieActors(actors):
-        """ Extracts the actors in the movie from url parsed
+    def extractGenres(genre, movies):
+        """Extracts movies of a user-specified genre"""
+        genre_movies = []
+        for movie in movies:
+            if genre in movie.genre:
+                genre_movies.append(movie)
+        return genre_movies
 
-        Args:
-            actors (str): The name of the actor that is being looked for from the url
 
-        Returns:
-            actors (list): A list of movies that match the actor which is picked
-        """
-        movies = []
+    def extractMovieActors(actor, movies):
+        """Extracts movies of a user-specified actor/actress"""
+        actor_movies = []
+        for movie in movies:
+            if actor in movie.actors:
+                actor_movies.append(movie)
+        return actor_movies
 
-        for movie in scrapeData(url):
-            if actors in movie.actors:
-                movies.append(movie)
+
+    def userChoice():
+        """Asks the user for favorite genre and actor/actress"""
+        genre = input("Enter your desired genre: ")
+        actor = input("Enter your preferred actor/actress: ")
+        return genre, actor
+
+
+    def movieRecs(genre, actor):
+        """Recommends movies based on the user's preferred genre and actor/actress"""
+        url = 'https://www.imdb.com/search/title/?title_type=feature&genres={}&start={}'
+        movies = Movie.scrape_movies(genre)
+
+        if genre:
+            movies = Movie.extractGenres(genre, movies)
+
+        if actor:
+            movies = Movie.extractMovieActors(actor, movies)
+
         return movies
 
-def userChoice():
-    """ Prompts the user to choose their search preference of genre or actor
-
-    Returns:
-        choice (str): The user's search preference
-
-    """
-    while True:
-        choice = input("Would you like to search by genre or actor? Enter 'genre' or 'actor': ")
-        if choice == 'genre':
-            return choice
-        elif choice == 'actor':
-            return choice
-        else:
-            print("Invalid choice. Please enter 'genre' or 'actor': ")
-
-def movieRecs(choice, genre, actor):
-    """ Recommends movies based on user's choice of genre or actor
-
-    Args:
-        choice (str): The user's search choice of genre or actor
-        genre (str): The movie genre searched for
-        actor (str): The actor searched for
-
-    Returns:
-        recs (list): A list of recommended movies that match the users choice
-
-    """
-    if choice == 'genre':
-        recommendations = Movie.extractGenres(genre)
-
-    else:
-        recommendations = Movie.extractMovieActors(actor)
-
-    titles = []
-    for movie in recommendations:
-        titles.append(Movie.extractTitles(title))
-
-    return recommendations
 
 def main():
-    """ The main method pulls all of the methods and functions to runs the code
-    """
-    url = 'https://www.themoviedb.org/?language=en-US'
-    userChoice = userChoice()
-    movie_data = scrapeData(url)
+    genre, actor = Movie.userChoice()
+    movies = Movie.movieRecs(genre, actor)
 
-    if userChoice =='genre':
-        genre = input('Enter a movie genre:')
-        movies = extractGenres(genre)
-        reccomendations = movieRecs(choice, genre, None)
-    else:
-        actor = input('Enter an actor/actress name:')
-        movies = extractMovieActors(actor)
-        reccomendations = movieRecs(choice, None, actor)
-
-    print('We recommend the following movies:')
-    for movie in reccomendations:
-        print(f"{movie.title}")
+    print('\nRecommended Movies:')
+    for movie in movies:
+        print(movie)
 
 
-# Unit Test for userChoice
-assert userChoice("genre") == "genre"
-assert userChoice("actor") == "actor"
-assert userChoice("invalid") == None
+if __name__ == '__main__':
+    main()
 
-# Unit Test for movieRecs
-assert movieRecs("genre", "", 'Science Fiction', '') == ["65", "Black Panther"]
-assert movieRecs("actor", "", "", "Leonardo DiCaprio") == ["Killers of the Flower Moon", "Don't Look Up", "Speed"]
-assert movieRecs("genre", "", "", "") == []
-
-
-#Test Function/Unit test for extractMovieActors
-def test_extractMovieActors():
-    #Instances of movies
-    movie1 = Movie("The Dark Knight", "Action", "Batman fights crime in Gotham City", ["Christian Bale", "Heath Ledger"])
-    movie2 = Movie("Interstellar", "Sci-Fi", "A team of astronauts travel through a wormhole", ["Matthew McConaughey", "Anne Hathaway"])
-    movie3 = Movie("The Prestige", "Drama", "Two magicians engage in a competitive rivalry", ["Christian Bale", "Hugh Jackman"])
-    movie4 = Movie("Inception", "Action", "A thief steals corporate secrets through dream-sharing technology", ["Leonardo DiCaprio", "Tom Hardy"])
-
-    allMovies = [movie1, movie2, movie3, movie4]
-
-
-    assert Movie.extractMovieActors("Christian Bale", allMovies) == [movie1, movie3]
-
-    assert Movie.extractMovieActors("Angelina Jolie", allMovies) == []
-
-#Unit Test for extractGenres
-assert extractGenres(Genre) == 'Horror'
-#Unit Test for extractSummary
-assert extractSummary(summary) == "A thief steals corporate secrets through dream-sharing technology"
-
-class testScrapeMovies(unittest.TestCase):
-    # this test checks if the method returns empty lists for actors, titles, etc and also checks if the HTTP request was successful
-    def testScrapeMovies(self):
-        url = 'https://www.imdb.com/chart/top'
-        resp,s, titles, genres, descrips, actors = scrape_movies(url)
-        self.assertIsInstance(s, BeautifulSoup)
-        self.assertEqual(len(titles), 0)
-        self.assertEqual(len(genres), 0)
-        self.assertEqual(len(descrips), 0)
-        self.assertEqual(len(actors), 0)
-        self.assertTrue(resp.ok)
-
-class testgetMovieTitles(unittest.TestCase):
-    def testMovieTitles(self):
-        htmlStructure = '''
-        <div class="Movie Title">
-            <a href="/movie1">
-                <span class="title">Movie 1</span>
-            </a>
-        </div>
-        <div class="Movie Title">
-            <a href="/movie2">
-                <span class="title">Movie 2</span>
-            </a>
-        </div>
-        '''
-        s = BeautifulSoup(htmlStructure, 'html.parser')  # parses mock html structure provided above
-        movieTitles = getMovieTitles(s)  # calls function
-        expected = ['Movie 1', 'Movie 2']  # expected data that was extracted
-        self.assertEqual(movieTitles, expected)  # compares if expected and actual output match
+class TestScrapeData(unittest.TestCase):
+    def testscrapeData(self):
+        m=Movie.scrape_movies('Horror',1)
+        self.assertIsInstance(m,list)
+        self.assertTrue(all(isinstance(mo,Movie) for mo in m))
+        self.assertEqual(len(m,50))
